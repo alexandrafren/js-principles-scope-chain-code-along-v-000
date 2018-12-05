b8c1ce8e6efe475d0a5aa39cc63a6df4f480bc0f
Alexandra Fren <35177685+alexandrafren@users.noreply.github.com>
2018-12-05 13:41:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'bob';
+
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "maybe not bob";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "definitely not bob";
+}
